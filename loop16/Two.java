0430d0386da6c8a7941b5068887326f53aa921ed
salonichoudhary <salonichoudhary112244@gmail.com>
2025-05-19 03:00:18 -0700

    java

---
+++ loop16/Two.java
@@ -0,0 +1,16 @@
+public class Two{
+    public static void main(String arr[]){
+         
+
+         // arthmetric series
+
+         int a=2;
+         int d=3;
+         int n=5;
+         for(int i=0;i<=10;i++){
+                 System.out.print(a+i*d);
+
+         }
+          
+    }
+}
