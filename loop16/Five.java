aff0ed20c133bb9ddcec8c5febc766f48181ff6b
salonichoudhary <salonichoudhary112244@gmail.com>
2025-05-23 00:42:08 -0700

    java

--- loop16/Five.java
+++ loop16/Five.java
@@ -19,12 +19,49 @@ public class Five{
 
 
 
+      //invert left
+
+        // for(int r=1;r<=5;++r){
+        //     for(int c=1;c<=5;++c){
+        //         if(c>=r){
+        //             System.out.print("*");
+        //         }
+        //         else{
+        //              System.out.print(" ");
+        //         }
+        //     }
+        //      System.out.println();
+        // }
 
 
+//left angle trangle
+
+//   for(int r=1;r<=5;++r){
+//             for(int c=1;c<=5;++c){
+//                 if(c>=6-r){
+//                     System.out.print("*");
+//                 }
+//                 else{
+//                      System.out.print(" ");
+//                 }
+//             }
+//              System.out.println();
+//         }
 
 
+//inverted right
 
-
+//  for(int r=1;r<=5;++r){
+//             for(int c=1;c<=5;++c){
+//                 if(c<=6-r){
+//                     System.out.print("*");
+//                 }
+//                 else{
+//                      System.out.print(" ");
+//                 }
+//             }
+//              System.out.println();
+//         }
 
 
 
@@ -57,13 +94,147 @@ public class Five{
 
         //dymond
  
-          int a=0;
-        for(int r=1;r<=9;++r){
+        //   int a=0;
+        // for(int r=1;r<=9;++r){
 
-            a=(r<=5)?++a:--a;
-            for(int c=1;c<=9;++c){
-                if(c>=6-a && c<=4+a){
-                    System.out.print("*");
+        //     a=(r<=5)?++a:--a;
+        //     for(int c=1;c<=9;++c){
+        //         if(c>=6-a && c<=4+a){
+        //             System.out.print("*");
+        //         }
+        //         else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
+//number print
+
+    //    int a=0;
+    //     for(int r=1;r<=5;++r){
+    //         for(int c=1;c<=5;++c){
+    //             if(c<=r){
+    //                 System.out.print(++a);
+    //             }
+    //             else{
+    //                 System.out.print(" ");
+    //             }
+    //         }
+    //         System.out.println();
+    //     }
+
+
+
+// number one se
+
+//  int a=0;
+//         for(int r=1;r<=5;++r){
+//             for(int c=1;c<=5;++c){
+//                 if(c<=r){
+//                     System.out.print(c);
+//                 }
+//                 else{
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+
+
+// one one two two number print
+
+
+        //  int a=0;
+        // for(int r=1;r<=5;++r){
+        //     for(int c=1;c<=5;++c){
+        //         if(c<=r){
+        //             System.out.print(r);
+        //         }
+        //         else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
+// A print
+
+
+    //   String a="A";
+    //     for(int r=1;r<=5;++r){
+    //         for(int c=1;c<=5;++c){
+    //             if(c<=r){
+    //                 System.out.print(a);
+    //             }
+    //             else{
+    //                 System.out.print(" ");
+    //             }
+    //         }
+    //         System.out.println();
+    //     }
+
+
+// abcd line se print
+//    char a='a';
+//         for(int r=1;r<=5;++r){
+//             for(int c=1;c<=5;++c){
+//                 if(c<=r){
+//                     System.out.print(a++);
+//                 }
+//                 else{
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+
+
+//capital A print
+
+
+
+//  char a='a';
+//         for(int r=1;r<=5;++r){
+//             for(int c=1;c<=5;++c){
+//                 if(c<=r){
+//                     System.out.print((char)(r+64));
+//                 }
+//                 else{
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+
+//small a print row me print
+
+//  char a='a';
+//         for(int r=1;r<=5;++r){
+//             for(int c=1;c<=5;++c){
+//                 if(c<=r){
+//                     System.out.print((char)(r+96));
+//                 }
+//                 else{
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+
+
+// colum me a print
+char a='a';
+        for(int r=1;r<=5;++r){
+            for(int c=1;c<=5;++c){
+                if(c<=r){
+                    System.out.print((char)(c+96));
                 }
                 else{
                     System.out.print(" ");
