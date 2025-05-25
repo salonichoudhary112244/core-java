f50c4584cbeaec95964549d51affbb797b89e3c2
salonichoudhary <salonichoudhary112244@gmail.com>
2025-05-25 05:31:22 -0700

    java

--- prectice/One.java
+++ prectice/One.java
@@ -32,14 +32,30 @@ public class One{
 
 //hcf
 
-int num1=12;
-int num2=16;
-int hcf=1;
+// int num1=12;
+// int num2=16;
+// int hcf=1;
 
-for(int i=1;i<=num1 && i<=num2; i++){
-    if(num1%i==0 && num2%i==0){
-        hcf=i;
+// for(int i=1;i<=num1 && i<=num2; i++){
+//     if(num1%i==0 && num2%i==0){
+//         hcf=i;
+//     }
+// }
+// System.out.print(hcf);
+//     }}
+
+//lcm
+
+int num1=12;
+int num2=18;
+
+int max=(num1>num2)?num1:num2;
+int i=max;
+while(true){
+    if(i%num1==0 && i%num2==0){
+       break;
     }
+    i=i+max;
 }
-System.out.print(hcf);
+System.out.print(i);
     }}
