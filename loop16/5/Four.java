31548a58a5b80adae5eec41ade471f25725fc760
salonichoudhary <salonichoudhary112244@gmail.com>
2025-05-25 05:23:45 -0700

    java

--- loop16/5/Four.java
+++ loop16/5/Four.java
@@ -18,15 +18,78 @@ public class Four{
 // calculaTOR
 
 
-int power=2;
-int base=4;
-int ans=1;
+// int power=2;
+// int base=4;
+// int ans=1;
 
-for(int i=1;i<=power;i++){
-    ans=ans*base;
-    System.out.println(ans);
+// for(int i=1;i<=power;i++){
+//     ans=ans*base;
+//     System.out.println(ans);
+// }
+
+
+
+
+//gp
+
+// nth=a*r^(n-1);
+// a=first term;
+// r=common ratio;
+// n=5;
+
+// int a=1;
+// int n=6;
+// int r=3;
+// double nterm=0;
+// for(int i=a;i<=n;i++){
+//     nterm=a*Math.pow(r,(n-1));
+// }
+// System.out.println(nterm);
+
+// int a=1;
+// int n=6;
+// int r=3;
+// double nterm=0;
+// for(int i=a;i<=n;i++){
+//     nterm=a*Math.pow(r,i);
+// }
+// System.out.println(nterm);
+
+
+// quve
+
+int a=1;
+int n=6;
+int r=3;
+for(int i=0;i<n;i++){
+    System.out.println(a);
+    a=r*a;
 }
-    }
- 
 
+//fabonacci series
+
+// int i=1;
+// int n=5;
+// int first=1;
+// int second=2;
+
+// while(i<=n){
+//     if(i==1){
+//         System.out.println(i);
+//         i++;
+//         continue;
+//     }
+//     if(i==2){
+//         System.out.println(i);
+//         i++;
+//         continue;
+//     }
+//     int next = first+second;
+//     first=second;
+//     second=next;
+//     i++;
+//     System.out.println(next);
+    
+//     }
+}
 }
---
+++ loop16/5/tempCodeRunnerFile.java
@@ -0,0 +1,4 @@
+nth=a*r^(n-1);
+// a=first term;
+// r=common ratio;
+// n=5;
