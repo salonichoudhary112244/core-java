3a48781d66b94b2e47238a2bd4e93d48d01fa698
salonichoudhary <salonichoudhary112244@gmail.com>
2025-05-29 08:44:26 -0700

    java

--- opretar/One.java
+++ opretar/One.java
@@ -3,11 +3,20 @@ import java.util.Scanner;
 public class One{
     public static void main(String arr[]){
     
-    Scanner obj=new Scanner(System.in);
-    System.out.println("enter value");
-    int a;
-    a=obj.nextInt();
-    System.out.println(a);
-    System.out.println("hello");
+    // Scanner obj=new Scanner(System.in);
+    // System.out.println("enter value");
+    // int a;
+    // a=obj.nextInt();
+    // System.out.println(a);
+    // System.out.println("hello");
+
+
+
+    int c=7;
+    int d=3;
+    System.out.println(c&d);
+    System.out.println(c|d);
+    System.out.println(c^d);
     }
+
 }
