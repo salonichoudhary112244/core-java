8b11d69fc340dd866994bf0eb8310942edc60bb7
salonichoudhary <salonichoudhary112244@gmail.com>
2025-05-29 00:01:38 -0700

    java

---
+++ opretar/One.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class One{
+    public static void main(String arr[]){
+    
+    Scanner obj=new Scanner(System.in);
+    System.out.println("enter value");
+    int a;
+    a=obj.nextInt();
+    System.out.println(a);
+    System.out.println("hello");
+    }
+}
