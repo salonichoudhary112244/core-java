4d4e26c6c0691341dbae737ebc3a8c3115af8f08
salonichoudhary <salonichoudhary112244@gmail.com>
2025-06-03 05:16:59 -0700

    java

--- arrey/One.java
+++ arrey/One.java
@@ -60,33 +60,41 @@ public class One{
     
 //sum of arrey
 
-    Scanner obj=new Scanner(System.in);
-    System.out.print("enter the size of arrey");
-    int size=obj.nextInt();
-    int arr[]=new int[size];
-    System.out.print("enter the value of arrey");
-    for(int i=0;i<arr.length;++i){
-        arr[i]=obj.nextInt();
-    }
-    for(int arr1:arr){
-        System.out.println(arr1);
-    }
-    int sum=0;
-    for(int i=0;i<arr.length;++i){
-        sum=sum+arr[i];
-    }
-    System.out.print(sum);
-
-
-
     // Scanner obj=new Scanner(System.in);
     // System.out.print("enter the size of arrey");
     // int size=obj.nextInt();
     // int arr[]=new int[size];
     // System.out.print("enter the value of arrey");
     // for(int i=0;i<arr.length;++i){
-    //    arr=arr1;
+    //     arr[i]=obj.nextInt();
     // }
+    // for(int arr1:arr){
+    //     System.out.println(arr1);
+    // }
+    // int sum=0;
+    // for(int i=0;i<arr.length;++i){
+    //     sum=sum+arr[i];
+    // }
+    // System.out.print(sum);
+
+
+
+ Scanner obj=new Scanner(System.in);
+    System.out.print("enter the size of arrey");
+    int size=obj.nextInt();
+    int arr[]=new int[size];
+    System.out.print("enter the value of arrey");
+   for(int i=0;i<arr.length;++i){
+        arr[i]=obj.nextInt();
+    }
+    for(int i=0;i<arr.length/2;++i){
+        int temp=arr[size-i-1];
+        arr[size-i-1]=arr[i];
+        arr[i]=temp;
+    }
+    for( int i:arr){
+        System.out.print(i);
+    }
     
 
     }
