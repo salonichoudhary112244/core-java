553c05d24086c0e011be21d1cd3773f23291c396
salonichoudhary <salonichoudhary112244@gmail.com>
2025-06-05 05:25:20 -0700

    java

--- arrey/Two.java
+++ arrey/Two.java
@@ -55,20 +55,22 @@ public class Two{
 //         System.out.print(" ");
 // }
 
-
-int arr[]={18,62,43,4,35};
+//assendig booble sort
+int arr[]={8,7,6,5,4};
 int n=arr.length;
 
 for(int i=0;i<n-1;++i){
-    for(int j=i+1;j<n-1;++j){
-        if(arr[i]>arr[j+1]){
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
+    for(int j=0;j<n-1;++j){
+        if(arr[j]>arr[j+1]){
+            int temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
         }
     }
-    System.out.print(arr[i]);
-    System.out.print(" ");
 }
+for( int a:arr)
+    System.out.print(a);
+    System.out.print(" ");
+
     }
 }
