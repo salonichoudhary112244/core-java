5e1098da0de215ba7908fda10ee0946fb0d9d26e
salonichoudhary <salonichoudhary112244@gmail.com>
2025-06-05 05:37:09 -0700

    java

--- arrey/Two.java
+++ arrey/Two.java
@@ -56,21 +56,42 @@ public class Two{
 // }
 
 //assendig booble sort
-int arr[]={8,7,6,5,4};
-int n=arr.length;
+// int arr[]={8,7,6,5,4};
+// int n=arr.length;
 
-for(int i=0;i<n-1;++i){
-    for(int j=0;j<n-1;++j){
-        if(arr[j]>arr[j+1]){
-            int temp=arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=temp;
+// for(int i=0;i<n-1;++i){
+//     for(int j=0;j<n-1;++j){
+//         if(arr[j]>arr[j+1]){
+//             int temp=arr[j];
+//             arr[j]=arr[j+1];
+//             arr[j+1]=temp;
+//         }
+//     }
+// }
+// for( int a:arr)
+//     System.out.print(a);
+//     System.out.print(" ");
+
+//     }
+// }
+
+
+
+// asending
+
+int arr[]={1,4,6,3,8};
+int size=arr.length;
+for(int i=0;i<size-1;++i){
+    for(int j=i+1;j<size;++j){
+        if(arr[i]>arr[j]){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
         }
     }
 }
-for( int a:arr)
+for(int a:arr){
     System.out.print(a);
-    System.out.print(" ");
-
-    }
 }
+    }
+    }
