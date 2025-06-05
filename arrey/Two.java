99391675becb43c48c90629008c6bc712946551c
salonichoudhary <salonichoudhary112244@gmail.com>
2025-06-05 05:38:36 -0700

    java

--- arrey/Two.java
+++ arrey/Two.java
@@ -79,11 +79,30 @@ public class Two{
 
 // asending
 
+// int arr[]={1,4,6,3,8};
+// int size=arr.length;
+// for(int i=0;i<size-1;++i){
+//     for(int j=i+1;j<size;++j){
+//         if(arr[i]>arr[j]){
+//             int temp=arr[i];
+//             arr[i]=arr[j];
+//             arr[j]=temp;
+//         }
+//     }
+// }
+// for(int a:arr){
+//     System.out.print(a);
+// }
+
+
+//desending
+
+
 int arr[]={1,4,6,3,8};
 int size=arr.length;
 for(int i=0;i<size-1;++i){
     for(int j=i+1;j<size;++j){
-        if(arr[i]>arr[j]){
+        if(arr[i]<arr[j]){
             int temp=arr[i];
             arr[i]=arr[j];
             arr[j]=temp;
