ccc1630156a80f0f0e83e9bb49fb5f65bc094edb
salonichoudhary <salonichoudhary112244@gmail.com>
2025-06-10 07:33:43 -0700

    java

--- arrey/Two.java
+++ arrey/Two.java
@@ -379,21 +379,32 @@ public class Two{
 
 // fequency
 
- int arr[]={3,2,1,2,3};
- int s=arr.length;
+//  int arr[]={3,2,1,2,3};
+//  int s=arr.length;
 
- for(int i=0;i<s;++i){
-  int count=1;
+//  for(int i=0;i<s;++i){
+//   int count=1;
 
-  for(int j=i+1;j<s;++j){
-    if(arr[i]==arr[j] && arr[i]!=-1){
-      count++;
-      arr[i]=-1;
-    }
+//   for(int j=i+1;j<s;++j){
+//     if(arr[i]==arr[j] && arr[i]!=-1){
+//       count++;
+//       arr[i]=-1;
+//     }
+//   }
+//   if(arr[i]!=-1){
+//   System.out.println(arr[i] +"is" + count);
+//  }
+//  }
+
+// one missing number
+
+int arr[]={1,3,5,7,9};
+int s=arr.length;
+
+for(int i=0;i<s-1;++i){
+  if(arr[i+1]-arr[i]>1){
+    System.out.println(arr[i]+1);
   }
-  if(arr[i]!=-1){
-  System.out.println(arr[i] +"is" + count);
- }
- }
+}
     }
     }
