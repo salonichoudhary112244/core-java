02dcf0a2e20cbb387d2e5717244142ae8de3ecc0
salonichoudhary <salonichoudhary112244@gmail.com>
2025-06-12 04:21:21 -0700

    java

---
+++ Function11-6/One.java
@@ -0,0 +1,22 @@
+//         //return typ
+
+// public class One{
+//     static int display(){
+//         return 10;
+//     }
+//     public static void main(String ar[]){
+//      System.out.print(display());
+//     }
+// }
+
+
+ // string type
+
+public class One{
+    static String display(){
+        return "vaishnavi";
+    }
+    public static void main(String ar[]){
+     System.out.print(display()+" " +"choudhary");
+    }
+}
