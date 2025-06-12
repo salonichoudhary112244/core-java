e6171cdc2be2c9ce642c364ee55eef6312fccfe1
salonichoudhary <salonichoudhary112244@gmail.com>
2025-06-12 04:26:19 -0700

    java

--- Function11-6/One.java
+++ Function11-6/One.java
@@ -12,11 +12,23 @@
 
  // string type
 
+// public class One{
+//     static String display(){
+//         return "vaishnavi";
+//     }
+//     public static void main(String ar[]){
+//      System.out.print(display()+" " +"choudhary");
+//     }
+// }
+
+
+// parametar with non return 
+
 public class One{
-    static String display(){
-        return "vaishnavi";
+    static void sum(int a,int b){
+     System.out.print(a+b);
     }
     public static void main(String ar[]){
-     System.out.print(display()+" " +"choudhary");
+    sum(10,11);
     }
 }
