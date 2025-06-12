020d67d5ac691141e966e83963388b5989a6ce50
salonichoudhary <salonichoudhary112244@gmail.com>
2025-06-12 05:21:49 -0700

    java

--- Function11-6/One.java
+++ Function11-6/One.java
@@ -84,20 +84,62 @@
 // }
 
 
-//if else
+// loop
 
-public class One{
-    static int sumArray(int arr[]){
-        int sum=0;
-        for(int i=0;i<arr.length;++i){
-           sum=sum+arr[i];
-        }
-        return sum;
-    }
-    public static void main(String ar[]){
+// public class One{
+//     static int primeNumber(int num){
+      
+//       for(int i=2;i<arr.length;++i){
+//         if(num%i==0){
+//            System.out.println("prime");
+//            return 0;
+//         }
+//         System.out.println("not a prime");
+//         return 0;
+//       }
+//     }
+//     public static void main(String ar[]){
      
-     int num[]={1,2,3,4,5};
-     int total=sumArray(num);
-     System.out.println("sum of arrey" + total);
+//      primeNumber(10);
+// }
+// }
+
+
+// factorial
+
+// public class One{
+//     static int multiPly(int num){
+      
+//         if(num==1){
+//            return 1;
+//         }
+//         return num*multiPly(num-1);
+//       }
+//     }
+//     public static void main(String args[]){
+     
+//      System.out.println(primeNumber(10));
+// }
+
+
+// fabbonaci series
+public class One{
+    static int fiBo(int num){
+      
+        if(num==0){
+           return 0;
+        }
+        else if(num==1){
+            return 1;
+        }
+        return fiBo(num-1)+fiBo(num-2);
+      }
     }
+    public static void main(String args[]){
+
+        int num=5;
+        for(int i=0;i<5;++i)
+     
+     System.out.println(fiBo(i));
 }
+
